5e7111beae39d06fb56aa17d5a90e82e5584dcdb
Claudio Fioretti <claudio.fioretti@docebo.com>
2025-01-04 00:02:20 +0100

    add dough to recipeAggregate response

--- internal/recipe-manager/domain/recipe.go
+++ internal/recipe-manager/domain/recipe.go
@@ -3,14 +3,14 @@ package domain
 import "github.com/google/uuid"
 
 type Recipe struct {
-	Id          int
-	Uuid        uuid.UUID
-	Name        string
-	Description string
-	Author      string
-	Dough       Dough
-	Topping     Topping
-	Steps       Steps
+	Id          int       `json:"id"`
+	Uuid        uuid.UUID `json:"uuid"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	Author      string    `json:"author"`
+	Dough       Dough     `json:"dough"`
+	Topping     Topping   `json:"topping"`
+	Steps       Steps     `json:"steps"`
 }
 
 type RecipeAggregate struct {
@@ -23,7 +23,14 @@ type SplitIngredients struct {
 	SplitTopping []Topping
 }
 
-type Dough struct{}
+type Dough struct {
+	Total  float64 `json:"total"`
+	Flour  float64 `json:"flour"`
+	Water  float64 `json:"water"`
+	Salt   float64 `json:"salt"`
+	EvoOil float64 `json:"evoOil"`
+	Yeast  float64 `json:"yeast"`
+}
 
 type Topping struct{}
 
--- internal/recipe-manager/infrastructure/mysql/recipe_repository.go
+++ internal/recipe-manager/infrastructure/mysql/recipe_repository.go
@@ -2,6 +2,7 @@ package mysql
 
 import (
 	"database/sql"
+	"encoding/json"
 
 	"recipe-manager/internal/recipe-manager/domain"
 
@@ -19,22 +20,26 @@ func NewMySqlRecipeRepository(db *sql.DB) *MySqlRecipeRepository {
 
 func (rr MySqlRecipeRepository) GetRecipeByUuid(recipeUuid uuid.UUID) (*domain.Recipe, error) {
 	var response domain.Recipe
+	var doughJSON string
 
-	query := `
-		SELECT id, uuid, name, author
-		FROM recipes
-		WHERE uuid = ?
-	`
+	query := `SELECT id, uuid, name, description, author, dough FROM recipes WHERE uuid = ?`
 	err := rr.db.QueryRow(query, recipeUuid).Scan(
 		&response.Id,
 		&response.Uuid,
 		&response.Name,
+		&response.Description,
 		&response.Author,
+		&doughJSON,
 	)
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			return &domain.Recipe{}, nil
 		}
+		return nil, err
+	}
+
+	err = json.Unmarshal([]byte(doughJSON), &response.Dough)
+	if err != nil {
 		return nil, err
 	}
 
